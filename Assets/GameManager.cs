ff9f810bd5914d590b7e74c2b9a1079c00cf3df9
Jonathan Yu <yujd2@mcmaster.ca>
2018-03-30 20:44:01 -0400

    horror

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public bool redlight; 
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Animator anim; 
+	public float speed;
+	private GameObject target; 
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		Screen.SetResolution ((int)Screen.width, (int)Screen.height, true);
+		target = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.position = target.transform.position;
+		if( Input.acceleration.y > -0.20f ){
+			anim.SetBool ("walking", true);
+			transform.Translate ( 0, 0, 1*speed*Time.deltaTime);
+		}
+		if( Input.acceleration.y < -0.40f ){
+			anim.SetBool ("walking", false);
+			print("Stop");
+
+		}
+	}
+}
