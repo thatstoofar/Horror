1444d87cb4ea8c472f5d2b008f0a58ae80e3a949
Tian Guo <tianqi33@hotmail.com>
2018-04-03 17:36:28 -0400

    Lightning flash
    basic lightning flash in lightning manager

---
+++ Assets/FlashTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashTransition : MonoBehaviour {
+
+	public Image image;
+	Color c;
+	bool increase;
+	// Use this for initialization
+	void Start () {
+		image.color = c;
+		c = Color.white;
+		c.a = 0f;
+		increase = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (increase == true) {
+			if (image.color.a > 0.8f) {
+				increase = false;
+			} else {
+				c.a += 0.2f;
+			}
+		} else {
+			c.a -= 0.05f;
+		}
+		image.color = c;
+	}
+}
---
+++ Assets/LightningManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightningManager : MonoBehaviour {
+	public ParticleSystem lightning;
+	public GameObject flashCanvas;
+	ParticleSystem currentLightning;
+	GameObject currentFlash;
+
+	float stoptimer;
+	bool activation; 
+	// Use this for initialization
+	void Start () {
+		stoptimer = 5f;
+		activation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activation == false) {
+			activate ();
+			activation = true;
+			stoptimer = 5f;
+		}
+
+		if (stoptimer < 0f) {
+			activation = false;
+			deactivate ();
+		}
+
+		stoptimer -= Time.deltaTime;
+		Debug.Log (activation);
+		Debug.Log (stoptimer);
+	}
+
+	void activate() {
+		currentLightning = Instantiate (lightning, new Vector3(0, 45, 0), new Quaternion());
+		currentFlash = (GameObject)Instantiate (flashCanvas); 
+	}
+
+	void deactivate() {
+		Destroy (currentLightning.gameObject);
+		Destroy (currentFlash.gameObject);
+	}
+
+}
